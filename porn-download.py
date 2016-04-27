dc10e28caa40d544db7cad5f6c776f7b03489a06
iliul <liul.stone@gmail.com>
2016-04-27 18:20:18 +0800

    update the basic function code

--- porn-download.py
+++ porn-download.py
@@ -3,14 +3,24 @@
 import re
 import os
 import sys
-import requests
+import wget
 import random
+import requests
 from bs4 import BeautifulSoup
 from urllib import request, parse
 
-COOKIES="-b language=cn_CN;watch_times=0"
-URL="http://email.91dizhi.at.gmail.com.9h4.space/view_video.php?viewkey=24bfc9f6c5ecad37daf7"
-USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"
+OUTDIR = "."
+URL = "http://email.91dizhi.at.gmail.com.9h4.space/view_video.php?viewkey=10dbdc2e848c104e5f3c"
+COOKIES = "-b language=cn_CN;watch_times=0"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36"
+
+def download_video(url, path, title, ext):
+    response = requests.get(url)
+    outfile = '{}/{}.{}'.format(path, title, ext)
+    if response.status_code == 200:
+        with open(outfile, 'wb') as f:
+            f.write(response.content)
+    return True
 
 def pick_a_chinese_proxy():
     content = request.urlopen(
@@ -28,75 +38,58 @@ def pick_a_chinese_proxy():
     return random.choice(all_proxies)
 
 def get_html(url):
-    '''
-    r = requests.get(url)
-    #print(r.content.decode('utf-8'))
+    
+    proxies = {}
+    extractor_proxy = pick_a_chinese_proxy()
+    #proxies['http'] = 'http://{}'.format(extractor_proxy)
+    print("Using Chinese proxy {}".format(extractor_proxy))
 
+    r = requests.get(url, proxies=proxies)
     html = r.content.decode('utf-8')
-    with open('./data','w') as f:
-        f.write(html)
-    '''
+    
+    r = re.search("so.addVariable\(\'file\',\'(.*)\'\);\n{1,10}", html)
+    vid = r.group(1)
+    if vid:
+        print('vid : {vid}'.format(vid=vid))
 
-    with open('./data', 'r') as f:
-        html = f.read()
-        r = re.search("so.addVariable\(\'file\',\'(.*)\'\);\n{1,10}", html)
-        vid = r.group(1)
-        if vid:
-            print('vid : {vid}'.format(vid=vid))
+    r = re.search("so.addVariable\(\'max_vid\',\'(.*)\'\);\n{1,10}", html)
+    max_vid = r.group(1)
+    if max_vid:
+        print('max_vid : {max_vid}'.format(max_vid=max_vid))
 
-        r = re.search("so.addVariable\(\'max_vid\',\'(.*)\'\);\n{1,10}", html)
-        max_vid = r.group(1)
-        if max_vid:
-            print('max_vid : {max_vid}'.format(max_vid=max_vid))
+    r = re.search("so.addVariable\(\'seccode\',\'(.*)\'\);\n{1,10}", html)
+    seccode = r.group(1)
+    if seccode:
+        print('seccode : {seccode}'.format(seccode=seccode))
 
-        r = re.search("so.addVariable\(\'seccode\',\'(.*)\'\);\n{1,10}", html)
-        seccode = r.group(1)
-        if seccode:
-            print('seccode : {seccode}'.format(seccode=seccode))
+    r = re.search("so.addVariable\(\'mp4\',\'(.*)\'\);\n{1,10}", html)
+    mp4 = r.group(1)
+    if mp4:
+        print('mp4 : {mp4}'.format(mp4=mp4))
 
-        r = re.search("so.addVariable\(\'mp4\',\'(.*)\'\);\n{1,10}", html)
-        mp4 = r.group(1)
-        if mp4:
-            print('mp4 : {mp4}'.format(mp4=mp4))
-
-        r = re.search(r'<title>\W{1,10}(.*)\n.*</title>', html)
-        title = r.group(1).replace(' ', '-')
-        if title:
-            print('title : {title}'.format(title=title))
+    r = re.search(r'<title>\W{1,10}(.*)\n.*</title>', html)
+    title = r.group(1).replace(' ', '-')
+    if title:
+        print('title : {title}'.format(title=title))
 
     jsonurl = "http://email.91dizhi.at.gmail.com.9h4.space" + "/getfile.php?VID=" + vid + "&mp4=" + mp4 + "&seccode=" + seccode + "&max_vid=" + max_vid
     print('jsonurl : {jsonurl}'.format(jsonurl=jsonurl))
 
-    """
-    proxies = {}
     ret = requests.get(jsonurl, proxies=proxies)
-    print(ret.content.decode('utf-8'))
-    with open('./link', 'w') as x:
-        x.write(ret.content.decode('utf-8'))
-    """
+    data = ret.content.decode('utf-8')
+    resp = re.search("^file=(.*)&domainUrl.*", data)
 
-    with open('./link', 'r') as x:
-        data = x.read()
-    resp = re.search("^(.*)&domainUrl.*", data)
-    if resp:
-        print("decode : {resp}".format(resp=resp.group(1)))
-        print("unquote : {resp}".format(resp=parse.unquote(resp.group(1))))
+    assert resp
+    print("decode : {resp}".format(resp=resp.group(1)))
+    print("unquote : {resp}".format(resp=parse.unquote(resp.group(1))))
+
+    download_url = parse.unquote(resp.group(1))
+    if download_video(download_url, OUTDIR, title, 'mp4'):    
+        print('download successful !!!')
+
+    #filename = wget.download(parse.unquote(resp.group(1)))
+    #filename = wget.download(parse.unquote('http://www.futurecrew.com/skaven/song_files/mp3/razorback.mp3'))
 
 if __name__ == "__main__":
     get_html(URL)
 
-    extractor_proxy = pick_a_chinese_proxy()
-    print("Using Chinese proxy {}".format(extractor_proxy))
-
-    qq = "http://www.qq.com"
-    facebook = "http://www.facebook.com"
-    #proxies = {"http" : "sock5://192.168.8.125:1080"}
-    proxies = {}
-    proxies['http'] = "http://" + pick_a_chinese_proxy()
-    print(proxies)
-
-    ret = requests.get(qq, proxies=proxies)
-    print(ret.status_code)
-    #print('text : %s' % ret.text)
-    #print(ret.headers)
-    #print(ret.request.headers)
